b0d28fe2a40fc5a32189d5a307ab0a19d9d01783
JobsWang <404780246@qq.com>
2015-08-11 11:46:43 +0800

    Initial Commit

---
+++ FMPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMPlayerDemo
+//
+//  Created by FENGWANG on 15/8/11.
+//  Copyright (c) 2015年 XIANFENGWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FMPlayerDemoTests/FMPlayerDemoTests.m
@@ -0,0 +1,40 @@
+//
+//  FMPlayerDemoTests.m
+//  FMPlayerDemoTests
+//
+//  Created by FENGWANG on 15/8/11.
+//  Copyright (c) 2015年 XIANFENGWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface FMPlayerDemoTests : XCTestCase
+
+@end
+
+@implementation FMPlayerDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
